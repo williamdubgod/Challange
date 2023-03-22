be93745e817874033fa5c35b3fb764c7ee2a0e88
William Vulcano <99343816+williamdubgod@users.noreply.github.com>
2023-03-22 17:42:55 -0300

    responsividade login

---
+++ web2/src/components/Home.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Homem from "../Img/Home/homem.png"
+import './style.css'
+import Cabecalho from "./Cabecalho";
+import Footer from "./Footer";
+
+export default function Home() {
+    return (
+        <body>
+            <Cabecalho />
+            <main id="conteudo-principal">
+
+                <article id="second-article">
+
+                    <section className="img-2">
+                        <figure className="img-2-2">
+                            <img src={Homem} alt={'Imagem de um homem feliz investindo'} id="aprenda-investir-img"/>
+                        </figure>   
+                    </section>
+
+                    <section id="frase-2">
+                        <aside>
+                            <h2 id="palavra-chave" className="frases-efeito"> Aprenda a investir</h2>
+                            <h2 className="frases-efeito">Enquanto você se diverte</h2>
+                            <h3>Cadastre-se e venha aprender na prática como <br/>
+                             você pode investir na bolsa de valores por meio do IPO </h3>
+                            <input type="button" id="cadastre-se" value="cadastre-se" action="cadastro.html" />
+                        </aside>
+                    </section>
+                </article>
+            </main>
+            <Footer />
+        </body>
+    )
+}
---
+++ web2/src/components/Simulacoes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Cabecalho from "./Cabecalho";
+import Footer from "./Footer";
+
+export default function Simulacoes() {
+
+    return (
+        <div id="simulacoes">
+            <Cabecalho />
+            <section id="conteudo-simulacoes">
+            <div id="slideshow">
+                <p class="magictime">Realiza o seu primeiro IPO</p>
+            </div>
+            </section>
+            <Footer />
+        </div>
+    )
+    
+}
